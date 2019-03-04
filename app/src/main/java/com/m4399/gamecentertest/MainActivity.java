4cc3ca9211ced2b8ccdd0e8ad83bd87e4699419c
Kattykai <Kattykai@163.com>
2019-03-04 22:27:58 +0800

    2019-3-4update

---
+++ app/src/main/java/com/m4399/gamecentertest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.m4399.gamecentertest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
